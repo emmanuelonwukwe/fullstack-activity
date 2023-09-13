777c402e8a922c2fb8d605321c731b7eeb9798a4
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2023-09-13 14:45:37 +0100

    Add delete functionality to the todo app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,41 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
-      //Fetching code here to set our todos
+      try {
+        //Fetching code here to set our todos
       const response = await fetch("http://localhost:3000/api/todos");
       const data = await response.json();
       setTodos(data);
+        
+      } catch (error) {
+        console.log("Error occured: " + error);
+      }
+      
     };
 
     fetchData();
   }, []);
 
-  async function addTodo() {
+  // This function fetches the todos from the backend
+  const fetchAndSetTodos = async () => {
+    //Fetching code here to set our todos
+    const response = await fetch("http://localhost:3000/api/todos");
+    const data = await response.json();
+    setTodos(data);
+  };
+
+  // This function helps to add the todo task to the list of todos
+  const handleAddTodoSubmit = async (event) => {
+    event.preventDefault();
+
+    if (task == '') {
+      throw new Error("Sorry, you can not create empty todo");
+    }
+
     try {
       const response = await fetch("http://localhost:3000/api/todos2", {
         method: "POST",
@@ -23,8 +45,8 @@ function App() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          task: "Wash clothes",
-          is_completed: false
+          task: task,
+          is_completed: false,
         }),
       });
 
@@ -32,11 +54,31 @@ function App() {
       const data = await response.json();
 
       // Set the todos after it had been returned
-      setTodos((oldTodos) => ([...oldTodos, ...data]))
-      console.log(data);
+      setTodos((oldTodos) => [...oldTodos, ...data]);
+      //console.log(data);
       // setting data
     } catch (error) {
-      console.log("Adding to todo error occured: " + error)
+      console.log("Adding to todo error occured: " + error);
+    }
+  };
+
+  // This function helps to add the todo task to the list of todos
+  const handleTodoDelete = async (todoId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/todos/delete/${todoId}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      if (response) {
+        // Set fetch and set the remaining todos
+        fetchAndSetTodos();
+      }
+      
+    } catch (error) {
+      console.log("Adding to todo error occured: " + error);
     }
   }
 
@@ -46,25 +88,32 @@ function App() {
         <h1 className="text-green-500 font-semibold text-2xl mt-4 mb-3">
           To-do list
         </h1>
-        <input
-          type="text"
-          placeholder="Enter todo"
-          className="bg-slate-500 text-white font-bold placeholder:text-green-200 border-2 rounded-xl p-2 border-solid border-black"
-        />
-        <button
-          type="button"
-          className="font-bold ml-1 bg-slate-500 rounded-lg p-2 text-white"
-          onClick={addTodo}
-        >
-          Add
-        </button>
+        <form onSubmit={handleAddTodoSubmit}>
+          <input
+            type="text"
+            placeholder="Enter todo"
+            onChange={(event) => setTask(event.target.value)}
+            value={task}
+            className="bg-slate-500 text-white font-bold placeholder:text-green-200 border-2 rounded-xl p-2 border-solid border-black"
+          />
+          <button
+            type="submit"
+            className="font-bold ml-1 bg-slate-500 rounded-lg p-2 text-white"
+          >
+            Add
+          </button>
+        </form>
         <ul className="mt-2">
           {todos.length > 0 ? (
             todos.map((todo, i) => (
-              <li key={i}>
-                <input type="checkbox" />
+              <li key={i} className="m-2 p-2 border border-t-2">
+                <input type="checkbox" className="mr-1" />
                 Id-{todo.id} {todo.task}
-                <button type="button" className="ml-2">
+                <button
+                  onClick={() => handleTodoDelete(todo.id)}
+                  type="button"
+                  className="ml-2 font-bold  bg-red-500 rounded-lg p-2 text-white"
+                >
                   Delete
                 </button>
               </li>
--- server/index.js
+++ server/index.js
@@ -10,35 +10,16 @@ app.use(express.json());
 app.use(cors({ origin: ["http://localhost:5173"] }));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // First query to get the version of the database
 async function getPgVersion() {
   const version = await sql`SELECT VERSION()`;
   return version;
 }
-
-// Dummy data for the todo
-const data = [
-  {
-    id: 1,
-    task: "Cook and eat",
-    is_completed: true,
-  },
-  {
-    id: 1,
-    task: "Play and smile",
-    is_completed: false,
-  },
-  {
-    id: 1,
-    task: "Pray and sleep",
-    is_completed: false,
-  },
-];
 
 app.get("/", async (req, res) => {
   try {
@@ -54,27 +35,44 @@ app.get("/api/todos", async (req, res) => {
   try {
     const todos = await sql`SELECT * FROM todos`;
     if (!todos) {
-        res.status(403).send("Unable to select items from todos");
-        return;
+      res.status(403).send("Unable to select items from todos");
+      return;
     }
 
     res.send(todos);
-      return;
+    return;
   } catch (error) {
     res.send(error);
   }
 });
 
 app.post("/api/todos2", async (req, res) => {
-    try {
-        const { task, is_completed } = req.body;
-        //const data = { task, is_completed };
-      //INSERT INTO todos ${sql(data, "task", "is_completed") RETURNING *
-        const todos = await sql`INSERT INTO todos (task, is_completed) VALUES (${task}, ${is_completed}) RETURNING *`;
-        res.json(todos); 
-    } catch (error) {
-        res.status(402).json(error);
-    }
+  try {
+    const { task, is_completed } = req.body;
+    //const data = { task, is_completed };
+    //INSERT INTO todos ${sql(data, "task", "is_completed") RETURNING *
+    const todos =
+      await sql`INSERT INTO todos (task, is_completed) VALUES (${task}, ${is_completed}) RETURNING *`;
+    res.json(todos);
+  } catch (error) {
+    res.status(402).json(error);
+  }
+});
+
+app.delete("/api/todos/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Delete the data from the db
+    await sql`DELETE FROM todos WHERE id=${id}`;
+
+    res.json({
+      status: "success",
+      message: "Successfully deleted"
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 app.get("*", (req, res) => {
