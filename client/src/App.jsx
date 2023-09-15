8598d50b85cb9647d6b1948a57b485ada901aeae
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2023-09-15 12:50:59 +0100

    Correct typo in update modal container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -251,7 +251,7 @@ function App() {
                   />
                 </svg>
                 <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                  Are you sure you want to edit this product id-
+                  Are you sure you want to edit this task id-
                   {todoToUpdate.id}?
                 </h3>
                 <form onSubmit={handleUpdateTodoSubmit}>
